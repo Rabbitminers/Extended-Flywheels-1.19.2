2821eb6fb1078d1ea53406c002d9fa3667813a22
Charlie Wood <rabbitminers2.0@gmail.com>
2022-09-26 00:14:00 +0100

    Initial commit

---
+++ src/main/java/com/rabbitminers/extendedflywheels/ExtendedFlywheels.java
@@ -0,0 +1,20 @@
+package com.rabbitminers.extendedflywheels;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.fml.common.Mod;
+import org.slf4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(ExtendedFlywheels.MODID)
+public class ExtendedFlywheels
+{
+    public static final String MODID = "extendedflywheels";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public ExtendedFlywheels()
+    {
+
+    }
+
+
+}
